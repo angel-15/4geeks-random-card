f94f98e0128cc54d1fc45f4b8fa5e0d909eeecbc
Angel Restifo <angelrestifo36@gmail.com>
2021-09-09 21:09:51 -0400

    random card utilizando js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let card = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let pintas = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomCard = () => {
+  let cardRandom = Math.floor(Math.random() * (card.length - 1));
+  let pintasRandom = Math.floor(Math.random() * (pintas.length - 1));
+  let cardNumber = card[cardRandom];
+  let nuevaPinta = pintas[pintasRandom];
+  let color = "";
+  console.log(pintasRandom);
+  if (pintasRandom === 0 || pintasRandom === 1) {
+    color = "red";
+  } else {
+    color = "black";
+  }
+  console.log(color);
+  let contenedorNumero = document.getElementById("number-type");
+  let contenedorPinta = document.querySelectorAll("#card-type");
+  console.log(contenedorPinta);
+  for (let i = 0; i < contenedorPinta.length; i++) {
+    contenedorPinta[i].innerHTML = nuevaPinta;
+    contenedorPinta[i].style.color = color;
+    contenedorPinta[i].style.fontSize = "18";
+  }
+
+  contenedorNumero.innerHTML = cardNumber;
+  contenedorNumero.style.color = color;
 };
+
+window.onload = generateRandomCard;
